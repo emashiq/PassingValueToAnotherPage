4bee37ea9f47a03024342e5e935e1b3717cc03be
Ashiqur Rahman Emran <em.ashiq@hotmail.com>
2017-07-09 00:01:15 +0600

    Work Done

---
+++ TestDriveValuePassToAnotherPage/Controllers/TestController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TestDriveValuePassToAnotherPage.Controllers
+{
+    public class TestController : Controller
+    {
+        public ActionResult SetValue()
+        {
+            return View();
+        }
+
+        public ActionResult SetValueInCookiesByJavaScript()
+        {
+            return View();
+        }
+        public ActionResult GetValueInCookiesByJavaScript()
+        {
+            return View();
+        }
+        // GET: Test
+        public ActionResult GetValue(string Value="")
+        {
+            ViewBag.Value = Value;
+            //URL DEMO Passing Through Get Method
+            //http://localhost:4385/Test/GetValue?Value=working
+            return View();
+        }
+
+        public ActionResult SetValueInLocalStorageByHTML5()
+        {
+            return View();
+        }
+        public ActionResult GetValueInLocalStorageByHTML5()
+        {
+            return View();
+        }
+
+    }
+}
